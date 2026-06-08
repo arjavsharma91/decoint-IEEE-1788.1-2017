46d2a6e85078dedbd5cd989ca622bedd2d4d7d5a
Arjav Sharma <arjav.sharma.91@gmail.com>
2026-06-08 19:02:08 -0400

    Update test_random.py
    added div test

--- testing/test_random.py
+++ testing/test_random.py
@@ -1,4 +1,5 @@
 from intervals.arithmetic import add, sub, mul, div
+from intervals.interval import Interval
 from tests.utils import rand_interval_mixed, sample, assert_contains
 
 def test_add_containment():
@@ -47,3 +48,23 @@ def test_reciprocal():
   result = reciprocal(x)
 
   assert_contains(result, 1 / a)
+
+def test_div_containment():
+  x = rand_interval_mixed()
+  y = rand_interval_mixed()
+
+  if y.is_empty:
+        return
+
+  result = div(x, y)
+
+  if y.contains(0):
+    assert result.is_entire or result == Interval.entire()
+    return
+  a = sample(x)
+  b = sample(y)
+
+  if b == 0:
+    return
+
+  assert_contains(result, a / b)
