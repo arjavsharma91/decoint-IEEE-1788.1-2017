389b0b66b140acdbbe5d12b248ecd52bc2980e92
Arjav Sharma <arjav.sharma.91@gmail.com>
2026-06-23 15:16:01 -0400

    Create decoration.py

---
+++ interval/decoration.py
@@ -0,0 +1,21 @@
+from enum import IntEnum
+
+class Decoration(IntEnum):
+  ILL = 0
+  TRV = 1
+  DEF = 2
+  DAC = 3
+  COM = 4
+
+  def __str__(self):
+    return self.name.lower()
+
+def combine(*decorations: Decoration) -> Decoration:
+  # IEEE 1788 requires that the minimum decoration has to be the one that is returned
+  if not decorations:
+    raise ValueError("combine() requires at least one decoration")
+
+  return min(decorations)
+
+
+  
