f30345a9f8b7c8802bae551e97a82bc53fe01b8b
Arjav Sharma <arjav.sharma.91@gmail.com>
2026-08-18 13:31:15 -0400

    Create pytest_trig_funcs.py

---
+++ testing/pytest_funcs/pytest_trig_funcs.py
@@ -0,0 +1,3 @@
+from decoint import Interval, DecoratedInterval, sin, asin, cos, acos, tan, atan, atan2
+import pytest
+from test_cases.test_exp_log_funcs import test_sin, test_sin_dec, test_cos, test_cos_dec, test_tan, test_tan_dec, test_asin, test_asin_dec, test_acos, test_acos_dec, test_atan, test_atan_dec, test_atan2, test_atan2_dec
