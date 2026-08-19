87d3664dbc30dc1c2c6cce80d11dfad0ceea238f
Arjav Sharma <arjav.sharma.91@gmail.com>
2026-08-19 12:38:15 -0400

    Create pytest_unary_properties.py

---
+++ testing/pytest_funcs/pytest_unary_properties.py
@@ -0,0 +1,3 @@
+from decoint import DecoratedInterval, Interval
+import pytest
+from test_cases.test_unary_properties import test_inf, test_inf_dec, test_sup, test_sup_dec, test_midpoint, test_midpoint_dec, test_radius, test_radius_dec, test_width, test_width_dec, test_magnitude, test_magnitude_dec, test_mignitude, test_mignitude_dec
