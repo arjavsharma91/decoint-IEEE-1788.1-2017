a23256101ab4a5083983fe199a3bfb1482a82e03
Arjav Sharma <arjav.sharma.91@gmail.com>
2026-08-25 01:32:35 -0400

    Update pytest_set_ops.py

--- testing/pytest_funcs/pytest_set_ops.py
+++ testing/pytest_funcs/pytest_set_ops.py
@@ -30,11 +30,14 @@ def hull_dec_testing(operand1, operand2, answer):
     op2 = DecoratedInterval(operand2)
     ans = DecoratedInterval(ans)
     actual = op1.hull(op2)
-    assert actual.interval.lo <= ans.interval.lo
-    assert actual.interval.lo >= pytest.approx(ans.interval.lo - 0.1)
-    assert actual.interval.hi >= ans.interval.hi
-    assert actual.interval.hi <= pytest.approx(ans.interval.hi + 0.1)
-    assert actual.decoration == ans.decoration
+    if ans.is_nai and actual.is_nai:
+        assert 1 == 1
+    else:
+        assert actual.interval.lo <= ans.interval.lo
+        assert actual.interval.lo >= pytest.approx(ans.interval.lo - 0.1)
+        assert actual.interval.hi >= ans.interval.hi
+        assert actual.interval.hi <= pytest.approx(ans.interval.hi + 0.1)
+        assert actual.decoration == ans.decoration
 
 @pytest.mark.parametrize("operand1, operand2, answer", test_intersection_dec)
 def intersection_dec_testing(operand1, operand2, answer):
@@ -42,8 +45,11 @@ def intersection_dec_testing(operand1, operand2, answer):
     op2 = DecoratedInterval(operand2)
     ans = DecoratedInterval(ans)
     actual = op1.intersection(op2)
-    assert actual.interval.lo <= ans.interval.lo
-    assert actual.interval.lo >= pytest.approx(ans.interval.lo - 0.1)
-    assert actual.interval.hi >= ans.interval.hi
-    assert actual.interval.hi <= pytest.approx(ans.interval.hi + 0.1)
-    assert actual.decoration == ans.decoration
+    if ans.is_nai and actual.is_nai:
+        assert 1 == 1
+    else:
+        assert actual.interval.lo <= ans.interval.lo
+        assert actual.interval.lo >= pytest.approx(ans.interval.lo - 0.1)
+        assert actual.interval.hi >= ans.interval.hi
+        assert actual.interval.hi <= pytest.approx(ans.interval.hi + 0.1)
+        assert actual.decoration == ans.decoration
